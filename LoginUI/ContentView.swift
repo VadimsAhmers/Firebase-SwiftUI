19b8779395ed283dbaceaf90cbdf253f22f3e980
VadimsAhmers <vadimspring@mail.ru>
2021-06-04 20:41:47 +0500

    Intermediate version, some issues are present

--- LoginUI/ContentView.swift
+++ LoginUI/ContentView.swift
@@ -9,11 +9,30 @@
 
 
 import SwiftUI
+import Firebase
+import FirebaseAuth
 
 struct ContentView: View {
+    @State var status = UserDefaults.standard.value(forKey: "status") as? Bool ?? false
+    
     var body: some View {
         
-        SignIn()
+        VStack {
+            if status {
+                Home()
+            }
+            else {
+                SignIn()
+            }
+        }
+        .animation(.spring())
+        .onAppear() {
+            NotificationCenter.default.addObserver(forName: NSNotification.Name("statusChange"), object: nil, queue: .main) {
+               (_) in
+                let status = UserDefaults.standard.value(forKey: "status") as? Bool ?? false
+                self.status = status
+            }
+        }
     }
 }
 
@@ -21,6 +40,9 @@ struct SignIn : View {
     
     @State var user = ""
     @State var pass = ""
+    @State var message = ""
+    @State var alert = false
+    @State var show = false
     
     var body : some View{
         VStack {
@@ -63,15 +85,30 @@ struct SignIn : View {
             VStack{
                 
                 Button(action: {
-                    
+                    signInWithEmail(email: self.user, password: self.pass) {
+                        (verified, status) in
+                        
+                        if !verified {
+                            self.message = status
+                            self.alert.toggle()
+                        }
+                        else {
+                            UserDefaults.standard.set(true, forKey: "status")
+                            NotificationCenter.default.post(name: NSNotification.Name("statusChange"), object: nil)
+                        }
+                    }
                 }) {
                     
                     Text("Sign In").foregroundColor(.white).frame(width: UIScreen.main.bounds.width - 120).padding()
                     
                     
-                }.background(Color("color"))
+                }.background(Color.green)
                 .clipShape(Capsule())
                 .padding(.top, 45)
+                
+                .alert(isPresented: $alert, content: {
+                    Alert(title: Text("Error"), message: Text(self.message), dismissButton: .default(Text("Ok")))
+                })
                 
                 Text("(or)").foregroundColor(Color.gray.opacity(0.5)).padding(.top,30)
                 
@@ -91,10 +128,127 @@ struct SignIn : View {
                     
                 }.padding(.top, 25)
             }
+            .sheet(isPresented: $show, content: {
+                SignUp(show: self.show)
+            })
         }
     }
 }
 
+struct SignUp : View {
+    
+    @State var user = ""
+    @State var pass = ""
+    @State var message = ""
+    @State var alert = false
+    @State var show = false
+    
+    var body : some View{
+        VStack {
+            Text("Sign Up").fontWeight(.heavy).font(.largeTitle).padding([.top,.bottom], 20)
+            VStack{
+                
+                VStack(alignment: .leading){
+                    
+                    VStack(alignment: .leading){
+                        
+                        Text("Username").font(.headline).fontWeight(.light).foregroundColor(Color.init(.label).opacity(0.75))
+                        
+                        HStack{
+                            
+                            TextField("Enter Your Username", text: $user)
+                            
+                            if user != ""{
+                                
+                                Image("check").foregroundColor(Color.init(.label))
+                            }
+                            
+                        }
+                        
+                        Divider()
+                        
+                    }.padding(.bottom, 15)
+                    
+                    VStack(alignment: .leading){
+                        
+                        Text("Password").font(.headline).fontWeight(.light).foregroundColor(Color.init(.label).opacity(0.75))
+                        
+                        SecureField("Enter Your Password", text: $pass)
+                        
+                        Divider()
+                    }
+                    
+                }.padding(.horizontal, 6)
+                
+            }.padding()
+            VStack{
+                
+                Button(action: {
+                    signUpWithEmail(email: self.user, password: self.pass) {
+                        (verified, status) in
+                        
+                        if !verified {
+                            self.message = status
+                            self.alert.toggle()
+                        }
+                        else {
+                            UserDefaults.standard.set(true, forKey: "status")
+                            NotificationCenter.default.post(name: NSNotification.Name("statusChange"), object: nil)
+                        }
+                    }
+                }) {
+                    
+                    Text("Sign Up").foregroundColor(.white).frame(width: UIScreen.main.bounds.width - 120).padding()
+                    
+                    
+                }.background(Color.green)
+                .clipShape(Capsule())
+                .padding(.top, 45)
+                
+                .alert(isPresented: $alert, content: {
+                    Alert(title: Text("Error"), message: Text(self.message), dismissButton: .default(Text("Ok")))
+                })
+            }
+        }
+    }
+}
+
+func signInWithEmail(email: String, password: String, completion: @escaping (Bool, String)->Void) {
+    
+    Auth.auth().signIn(withEmail: email, password: password) {(res, err) in
+        if err != nil {
+            completion(false, (err?.localizedDescription)!)
+            return
+        }
+        completion(true, (res?.user.email)!)
+    }
+}
+
+func signUpWithEmail(email: String, password: String, completion: @escaping (Bool, String)->Void) {
+    
+    Auth.auth().createUser(withEmail: email, password: password) {(res, err) in
+        if err != nil {
+            completion(false, (err?.localizedDescription)!)
+            return
+        }
+        completion(true, (res?.user.email)!)
+    }
+}
+
+
+struct Home: View {
+    var body: some View {
+        VStack {
+            Text("Home")
+            Button(action: {
+                UserDefaults.standard.set(false, forKey: "status")
+                NotificationCenter.default.post(name: NSNotification.Name("statusChange"), object: nil)
+            }, label: {
+                Text("Logout")
+            })
+        }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
